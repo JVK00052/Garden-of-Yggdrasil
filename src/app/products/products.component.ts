598bb00ef654477311dc1469d148e3a8c30c38ed
unknown <JVK00052@gmail.com>
2018-11-28 09:17:13 -0500

    API + AUTH

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { DataService } from '../data.service';
+import { DataService } from '../db/data.service';
+import { Products } from '../models/product';
 
 @Component({
   selector: 'app-products',
@@ -7,16 +8,25 @@ import { DataService } from '../data.service';
   styleUrls: ['./products.component.css']
 })
 export class ProductsComponent implements OnInit {
-  products: Object;
+  products: Products[];
 
-  constructor(private data: DataService) { }
+  constructor(private dataService: DataService) { }
 
   ngOnInit() {
-    this.data.getProducts().subscribe(data => {
-      this.products = data
-      console.log(this.products)
-    })
+    this.getProducts();
+  }
+  getProducts() : void {
+    this.dataService.getProducts().subscribe(Products => this.products = Products)
   }
 
+  delete(id){
+    if (localStorage.getItem('token')){
+      this.dataService.delete(id).subscribe(
+        id => this.getProducts()
+      )
+    } else {
+      alert('You are neither Thor, nor Loki, nor Hela. Mortal you have no power here.')
+    }
+  }
   
 }
